822091c9e998853080e6fbba34c0d5c73bbcb9e8
Max Scher <maxobaxo@gmail.com>
2017-10-04 15:33:50 -0700

    add actions for getGames() to be called upon submitted selected skills

---
+++ src/actions/games.js
@@ -0,0 +1,24 @@
+import * as types from './../constants/ActionTypes';
+import fetch from 'isomorphic-fetch';
+import { v4 } from 'uuid';
+
+export const requestGames = () => ({
+  type: types.REQUEST_GAMES,
+});
+
+export const receiveGames = () => ({
+  type: types.RECEIVE_GAMES,
+});
+
+
+export function getGames(selectedSkills, dispatch) {
+  return function(dispatch) {
+    dispatch(requestGames(selectedSkills));
+    return fetch('http://localhost:3000/games').then(
+      response => response.json(),
+      error => console.log('An error occured.', error)
+    ).then(function(json) {
+      console.log(json);
+    })
+  }
+}
