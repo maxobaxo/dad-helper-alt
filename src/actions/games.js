4f592d0e4db4405282497c45b6cc8ba5727188a0
Max Scher <maxobaxo@gmail.com>
2017-10-05 08:29:25 -0700

    create if/else statement to manage selectedSkills with no gameIds

--- src/actions/games.js
+++ src/actions/games.js
@@ -18,23 +18,29 @@ export function getGames(selectedSkills, dispatch) {
     dispatch(requestGames(selectedSkills));
     let gameIdsFromSkills = [];
     selectedSkills.forEach((skill) => {
-      skill.gameIds.forEach((gameId) => {
-        gameIdsFromSkills.push(gameId);
-      });
-    });
-    return fetch('http://localhost:3000/games').then(
-      response => response.json(),
-      error => console.log('An error occured.', error)
-    ).then(function(all_games) {
-      let games = [];
-      all_games.forEach((game) => {
-        gameIdsFromSkills.forEach((gameId) => {
-          if (gameId === game._id) {
-            games.push(game);
-          }
+      if (skill.gameIds) {
+        skill.gameIds.forEach((gameId) => {
+          gameIdsFromSkills.push(gameId);
         });
-      });
-      dispatch(receiveGames(games));
-    })
+        return fetch('http://localhost:3000/games').then(
+          response => response.json(),
+          error => console.log('An error occured.', error)
+        ).then(function(all_games) {
+          let games = [];
+          all_games.forEach((game) => {
+            gameIdsFromSkills.forEach((gameId) => {
+              if (gameId === game._id) {
+                games.push(game);
+              }
+            });
+          });
+          dispatch(receiveGames(games));
+        })
+      } else {
+        let noGames = [ { _id: "no_games_for_the_chosen_skills", name: "Our Apologies", description: "Unfortunately, we do not currently have any games for the skills you selected.", bonus: "We hope to have new games added for you soon." } ];
+        dispatch(receiveGames(noGames));
+      }
+    });
+
   }
 }
