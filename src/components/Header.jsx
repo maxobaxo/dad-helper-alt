6cdd529fb7ecad956b76ca6009b91a45c300e9bc
Max Scher <maxobaxo@gmail.com>
2018-07-24 15:51:17 -0700

    implement new appbar header with roboto font + mui icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,16 @@
-import React from 'react';
-import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
-import styles from './../styles/Header.css';
+import React from "react";
+import { Jumbotron, Grid, Row, Col } from "react-bootstrap";
+import { AppBar } from "react-toolbox/lib/app_bar";
+import styles from "./../styles/Header.css";
 
 function Header(props) {
-
-  return(
-    <div >
-      <Jumbotron className={styles.header}>
-        <Grid>
-          <Row>
-            <Col md={12}>
-              <h1>Get To Know Your Baby</h1>
-            </Col>
-          </Row>
-        </Grid>
-      </Jumbotron>
-      <div className="well">
-        <div>
-          <h4>Are you a new parent?</h4>
-          <p>Learn how to celebrate your child's achievements, and empower them to reach their developmental milestones.</p>
-        </div>
-      </div>
-    </div>
-  )
+  return (
+    <AppBar
+      className={styles.header}
+      title="Get To Know Your Baby"
+      leftIcon="child_friendly"
+    />
+  );
 }
 
 export default Header;
