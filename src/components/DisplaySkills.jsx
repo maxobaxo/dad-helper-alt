28e9ef9df5f8b3ab50f92a7450262766189ee330
Max Scher <maxobaxo@gmail.com>
2018-07-25 15:45:23 -0700

    use react-toolbox for checkboxes

--- src/components/DisplaySkills.jsx
+++ src/components/DisplaySkills.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { func, array } from "prop-types";
-import { Button, Panel, Accordion } from "react-bootstrap";
+import { Button } from "react-toolbox/lib/button";
+import { Card, CardTitle, CardMedia } from "react-toolbox/lib/card";
+
 import SkillCheckbox from "./SkillCheckbox";
 
 const propTypes = {
@@ -21,26 +23,28 @@ const DisplaySkills = ({
   );
 
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <Accordion>
-          <Panel
-            bsStyle="success"
-            header="Which of these skills has your baby mastered?"
-            footer={submitButton}
+    <div style={{ marginTop: "25px", marginBottom: "25px" }}>
+      <Card>
+        <CardTitle title="Which of these skills has your baby mastered?" />
+        <form onSubmit={handleFormSubmit}>
+          {babySkills.map((skill, index) => (
+            <SkillCheckbox
+              skill={skill}
+              skillName={skill.name}
+              skillId={skill._id}
+              handleCheckboxChange={handleCheckboxChange}
+              key={index}
+            />
+          ))}
+          <Button
+            style={{ marginTop: "25px", marginLeft: "25px" }}
+            type="submit"
+            raised
           >
-            {babySkills.map((skill, index) => (
-              <SkillCheckbox
-                skill={skill}
-                skillName={skill.name}
-                skillId={skill._id}
-                handleCheckboxChange={handleCheckboxChange}
-                key={index}
-              />
-            ))}
-          </Panel>
-        </Accordion>
-      </form>
+            Submit
+          </Button>
+        </form>
+      </Card>
     </div>
   );
 };
